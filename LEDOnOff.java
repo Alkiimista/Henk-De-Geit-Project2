893a3784b06fb4908eb9651fdd226ea322dcc24e
Alkiimista <dducnguyen97@gmail.com>
2015-11-27 12:58:24 +0100

    Update LEDOnOff.java

--- LEDOnOff.java
+++ LEDOnOff.java
@@ -1,11 +1,23 @@
+import TI.*;
 
-
-public class LEDOnOff
+public class LEDOnOff extends LED
 {
-
-    public LEDOnOff()
+    public LEDOnOff(int redPin, int greenPin, int bluePin)
     {
-        
+        super(redPin, greenPin, bluePin);
     }
 
+    public void turnOn()
+    {
+        red.update(5);
+        blue.update(5);
+        green.update(5);
+    }
+
+    public void turnOff()
+    {
+        red.update(255);
+        blue.update(255);
+        green.update(255);
+    }
 }
